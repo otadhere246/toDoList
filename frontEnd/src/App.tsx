b58e39430d4b65a31ec13b66295b89d72960e7d1
Najib Mohamed <najipyare64@gmail.com>
2025-01-06 08:34:23 +0300

    latest update

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -4,15 +4,15 @@ import './App.css'
 import MyComponent from './component/MyComponent';
 
 function App() {
- 
+  console.log("from najib")
   const name = 'ali';
 
   return (
     <>
-    <h1>my name {name}</h1>
-    <MyComponent name={name}/>
-    <MyComponent name={name}/>
-    <MyComponent name={name}/>
+      <h1>my name {name}</h1>
+      <MyComponent name={name} />
+      <MyComponent name={name} />
+      <MyComponent name={name} />
     </>
   )
 }
