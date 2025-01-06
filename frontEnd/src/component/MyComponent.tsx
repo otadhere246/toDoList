ee92bbb9b4e999820bfa08f04670ddcc2d20a593
khaled <otadhere246@gmail.com>
2025-01-06 08:19:45 +0300

    second commit

--- frontEnd/src/component/MyComponent.tsx
+++ frontEnd/src/component/MyComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MyComponent(Props) {
   return (
     <div>
-      <h2>from component{Props.name}</h2>
+      <h2>from  component{Props.name}</h2>
     </div>
   )
 }
